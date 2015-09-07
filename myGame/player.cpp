6b7be10569067c51c948525cea2e805fb779748e
codedecde <barunpatra95@gmail.com>
2015-09-08 00:08:31 +0530

    compilable code. TODO: alpha beta pruning

---
+++ myGame/player.cpp
@@ -0,0 +1,226 @@
+#ifndef PLAYER_CPP
+#define PLAYER_CPP
+
+#include "player.hpp"
+using namespace std;
+
+float bot::utilityFunction(){
+	// DO SOME MAGIC HERE
+	float theMagicValue = score;
+	return theMagicValue;
+}
+
+void bot::playAsOrder(char TileUsedByChaos,int row,int col){
+	board[row][col] = TileUsedByChaos;
+	unvisited_cells.erase(n*row + col);
+	tilePos.push_back(make_pair(row,col));
+	//TODO: UPDATE SCORE 
+	float dummy = expectiminimax(0,0);
+	printf("%d %d %d %d\n",initx,inity,finx,finy);
+}
+
+void bot::playAsChaos(char TileColorGiven){
+	tileToMinNode = TileColorGiven;
+	float dummy = expectiminimax(2,0);
+	printf("%d %d\n",posx,posy);
+}
+
+float bot::expectiminimax(int state, int depth){
+
+	if(depth == maxdepth) //Base Case
+		return utilityFunction();
+	
+	if(state == 0){ 
+
+		float globalmax = MINF;
+		// This is max node
+		for(int ii = 0;ii<tilePos.size();ii++){
+			
+			int row = tilePos[ii].first;
+			int column = tilePos[ii].second;
+			
+			vector<triple*> score_movements;
+			
+			int i = 1;
+			
+			bool stop_one = false,stop_two =false;
+			
+			while(!stop_one || !stop_two){
+				
+				if(column + i < n && board[row][column+i] == 'W' && !stop_one){
+
+					//setScoreDelta 
+					float scoreDelta;
+					score_movements.push_back(new triple(scoreDelta,row,column+i));
+					
+				}
+				else{
+					stop_one = true;
+				}
+				if(column - i >= 0 && board[row][column - i] == 'W' && !stop_two){
+					
+					//setScoreDelta
+					float scoreDelta;
+					score_movements.push_back(new triple(scoreDelta,row,column-i));
+					
+
+				}else{
+					stop_two = true;
+				}
+				i++;
+			}
+			
+			i = 1;
+			
+			stop_one = false; stop_two = false;
+			
+			while(!stop_one || !stop_two){
+				
+
+				if(row + i < n && board[row+i][column] == 'W' && !stop_one){
+					//setScoreDelta
+					float scoreDelta;
+					score_movements.push_back(new triple(scoreDelta,row+i,column));
+					
+				}else{
+					stop_one = true;
+				}
+				if(row - i >= 0 && board[row-i][column] == 'W' && !stop_two){
+					//setScoreDelta
+					float scoreDelta;
+					score_movements.push_back(new triple(scoreDelta,row-i,column));
+					
+					
+				}else{
+					stop_two = true;
+				}
+				i++;
+			}
+			
+			//TODO: Change for move ordering : sort(score_movements.begin(),score_movements.end(),comparison)
+
+
+			for (int i=0;i<score_movements.size();i++){
+				// make movement
+				int newrow = score_movements[i]->row;
+				int newcol = score_movements[i]->column;
+				float scoreDelta = score_movements[i]->score;
+				
+				score += scoreDelta;
+
+				//Memory management: score_management[i] no longer needed. Deleted. 
+				delete score_movements[i];
+				//End Memory management
+				
+				tilePos[ii].first = newrow;
+				tilePos[ii].second = newcol;
+				
+				board[newrow][newcol] = board[row][column];
+				unvisited_cells.erase(newrow*n + newcol);
+				board[row][column] = 'W';
+				unvisited_cells.insert(row*n + column);
+				
+				int localstate = state;
+				state  = (state + 1)%3;
+				float tempval = expectiminimax(state,depth+1);
+				
+				//Restoring board
+
+				state = localstate;
+				score -= scoreDelta;
+				
+				tilePos[ii].first = row;
+				tilePos[ii].second = column;
+				
+				board[row][column] = board[newrow][newcol];
+				unvisited_cells.erase(row*n + column);
+				board[newrow][newcol] = 'W';
+				unvisited_cells.insert(newrow*n + newcol);
+
+				if(tempval > globalmax){
+					// Store action that lead to this state
+					globalmax = tempval;
+					initx = row;
+					inity = column;
+					finx = newrow;
+					finy = newcol;
+					
+				}
+
+
+			}
+
+
+		}
+		return globalmax;
+	}
+	
+	else if(state == 1){
+		// This is chance node
+		float expectedvalue = 0.0;
+		for(int i=0;i<5;i++){
+			float prob = float(colors[i]) / totaltilesleft;
+			colors[i]--;
+			totaltilesleft--;
+			tileToMinNode = 65+i; //given color.
+			int localstate = state;
+			state = (state + 1)%3;
+			if(prob>0.0001)
+				expectedvalue += prob*expectiminimax(state,depth+1);
+			colors[i]++;
+			totaltilesleft++;
+		}
+		return expectedvalue;
+
+	}
+
+	else{
+		//This is min node
+		float globalmin = INF;
+		vector<triple*> score_movements;
+		for(unordered_set<int>::const_iterator it = unvisited_cells.begin();it!= unvisited_cells.end();it++){
+			int row =  (*it) / n;
+			int col = (*it) % n;
+			//Set scoreDelta
+			float scoreDelta;
+			score_movements.push_back(new triple(scoreDelta,row,col));
+		}
+		//TODO: Order moves: sort(score_movements.begin(),score_movements.end(),comparison)
+		for(int i=0;i<score_movements.size();i++){
+			//make movement
+			int row = score_movements[i]->row;
+			int col = score_movements[i]->column;
+			float scoreDelta = score_movements[i]->score;
+			
+			//Memory management: score_management[i] no longer needed. Deleted. 
+			delete score_movements[i];
+			//End Memory management
+			
+			score+=scoreDelta;
+			board[row][col] = tileToMinNode;
+			unvisited_cells.erase(n*row + col);
+
+			int localstate = state;
+			state = (state+1)%3;	
+			float localmin = expectiminimax(state,depth+1);
+			state = localstate;
+			//Restore board
+			board[row][col] = 'W';
+			unvisited_cells.insert(n*row + col);
+			score-=scoreDelta;
+			
+			if(localmin < globalmin){
+				localmin = globalmin;
+				tilecolor = tileToMinNode;
+				posx = row;
+				posy = col;
+			}
+
+		}
+		return globalmin;
+	}
+
+}
+
+
+#endif
--- myGame/player.hpp
+++ myGame/player.hpp
@@ -1,14 +1,44 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <unordered_set>
+#include <limits>
 using namespace std;
 
-#define INF numeric_limits<float>::max
-#define MINF numeric_limits<float>::min
+#define INF numeric_limits<float>::max()
+#define MINF numeric_limits<float>::min()
+
+struct triple{
+	float score;int row;int column;
+	triple(float a,int b,int c){
+		score = a;row = b;column = c;
+	}
+	void print(){
+		printf("%f %d %d\n",score,row,column);
+	}
+};
+
+
+struct comparator_structure{
+	bool operator()(triple* a,triple* b){
+		if(a && b){
+			if(a->score > b->score)
+				return true;
+			else
+				return false;
+		}
+		else{
+			printf("Either one of the values is null. Check \n");
+			return false;
+		}
+	}
+};
+
 
 class bot{
-	int sizeOfBoard;
+	int n;
 	vector<vector<char> > board; // The board
 	vector<int> colors; // Number of tiles left of valid colors
 	vector<pair<int,int> > tilePos; //The position of tiles.
@@ -17,116 +47,38 @@ class bot{
 	int totaltilesleft; // number of tiles left
 	char tileToMinNode;
 	int score;
-	void score(){
-		
+	unordered_set<int> unvisited_cells;
+	comparator_structure comparison;
+	void setScore(){
+	// TODO: RESET SCORE	
 	}
+	
+	//Book keeping for maintaining actions
+	
+	//Order
+	int initx,inity,finx,finy;
+	
+	//Chaos
+	char tilecolor;
+	int posx,posy;
+
 public:
-	bot(int n,bool ord):board(n,vector<char>(n,'')){
-		order = ord;
-		sizeOfBoard = n;
-		colors.resize(26,0);
+	
+	bot(int n,bool ord):board(n,vector<char>(n,'W')){
+		order = ord; // Unnecessary ? 
+		this->n = n;
+		colors.resize(n,n);
 		maxdepth = 3; // Vary this to get different depths.
 		totaltilesleft = 25;
-		tileToMinNode = '';
+		tileToMinNode = 'W';
 		score = 0;
+		initx=inity=finx=finy = posx=posy = -1;
+		for(int i=0;i<n*n;i++)
+			unvisited_cells.insert(i);
 	}
-	float expectiminimax(int state, int depth){
-		if(depth == maxdepth)
-			return utilityFunction();
-		if(state == 0){ 
-			float globalmax = MINF;
-			// This is max node
-			for(int i = 0;i<tilePos.size();i++){
-				int column = get<0>(tilePos[i]);
-				int row = get<1>(tilePos[i]);
-				vector<int> columnMovements;
-				vector<int> rowMovements;
-				int i = 1;
-				flag = true;
-				while(flag){
-					flag = false;
-					if(column + i < sizeOfBoard && board[row][column+i] == ''){
-						columnMovements.push_back(column + i);
-						flag = true;
-					}
-					if(colume - i >= 0 && board[row][column - i] == ''){
-						columnMovements.push_back(column-i);
-						flag = true;
-					}
-					if(flag==true)
-						i++;
-				}
-				i = 1;
-				flag = false;
-				while(flag){
-					flag = false;
-					if(row + i < sizeOfBoard && board[row+i][column] == ''){
-						rowMovements.push_back(row + i);
-						flag = true;
-					}
-					if(row - i >= 0 && board[row-i][column] == ''){
-						rowMovements.push_back(row-i);
-						flag = true;
-					}
-					if(flag==true)
-						i++;
-				}
-				//for valid movements of tile on board{
-					// make movement
-					int localstate = state;
-					state  = (state + 1)%3;
-					float tempval = expectiminimax(state,depth+1);
-					state = localstate;
-					if(tempval > globalmax){
-						globalmax = tempval;
-						// Store action that lead to this state
-					}
-					//Restore board
-					//}
-			}
-			return globalmax;
-		}
-		else if(state == 1){
-			// This is chance node
-			float expectedvalue = 0.0;
-			for(int i=0;i<5;i++){
-				float prob = float(colors[i]) / totaltilesleft;
-				colors[i]--;
-				totaltilesleft--;
-				tileToMinNode = 65+i; //given color.
-				int localstate = state;
-				state = (state + 1)%3;
-				if(prob>0.0001)
-					expectedvalue += prob*expectiminimax(state,depth+1);
-				colors[i]++;
-				totaltilesleft++;
-			}
-			return expectedvalue;
-
-		}
-		else{
-			//This is min node
-			float globalmin = INF;
-			//for valid moves of min nodes with tileToMinNode{
-				//make movement
-				int localstate = state;
-				state = (state+1)%3;	
-				float localmin = expectiminimax(state,depth+1);
-				state = localstate;
-				if(localmin < globalmin){
-					localmin = globalmin;
-					//Store action to this state
-				}
-				//Restore board
-
-			//}
-			return globalmin;
-		}
-	}	
-	float utilityFunction(){
-		// DO SOME MAGIC HERE
-		return 0.0;
-	}
+	
+	float expectiminimax(int state,int depth);	
+	float utilityFunction();
 	void playAsOrder(char TileUsedByChaos,int row,int col);
 	void playAsChaos(char TileColorGiven);
 };

11dbb2affedcea3792cbb486b0e9232ab920580e
codedecde <barunpatra95@gmail.com>
2015-09-08 12:41:24 +0530

    some more code

--- myGame/driver.cpp
+++ myGame/driver.cpp
@@ -4,7 +4,26 @@ using namespace std;
 
 
 int main(){
-	
-	bot mBot(5,false);
+	int N; string player;
+	cin>>N;
+	cin>>player;
+	int i = 0;
+	bool order = (player == "ORDER");
+	bot mBot(N,order);
+	while(i<N*N){
+		if(order){
+			int row,col;
+			char C;
+			cin>>row>>col>>C;
+			playAsOrder(C,row,col);
+			i++;
+		}
+		else{
+			char C;
+			cin>>C;
+			playAsChaos(C);
+			i++;
+		}
+	}
 	return 0;
 }
--- myGame/player.cpp
+++ myGame/player.cpp
@@ -52,7 +52,7 @@ float bot::expectiminimax(int state, int depth){
 				
 				if(column + i < n && board[row][column+i] == 'W' && !stop_one){
 
-					//setScoreDelta 
+					//TODO: setScoreDelta 
 					float scoreDelta;
 					score_movements.push_back(new triple(scoreDelta,row,column+i));
 					
@@ -62,7 +62,7 @@ float bot::expectiminimax(int state, int depth){
 				}
 				if(column - i >= 0 && board[row][column - i] == 'W' && !stop_two){
 					
-					//setScoreDelta
+					//TODO: setScoreDelta
 					float scoreDelta;
 					score_movements.push_back(new triple(scoreDelta,row,column-i));
 					
@@ -81,7 +81,7 @@ float bot::expectiminimax(int state, int depth){
 				
 
 				if(row + i < n && board[row+i][column] == 'W' && !stop_one){
-					//setScoreDelta
+					//TODO: setScoreDelta
 					float scoreDelta;
 					score_movements.push_back(new triple(scoreDelta,row+i,column));
 					
@@ -89,7 +89,7 @@ float bot::expectiminimax(int state, int depth){
 					stop_one = true;
 				}
 				if(row - i >= 0 && board[row-i][column] == 'W' && !stop_two){
-					//setScoreDelta
+					//TODO: setScoreDelta
 					float scoreDelta;
 					score_movements.push_back(new triple(scoreDelta,row-i,column));
 					
@@ -184,7 +184,7 @@ float bot::expectiminimax(int state, int depth){
 		for(unordered_set<int>::const_iterator it = unvisited_cells.begin();it!= unvisited_cells.end();it++){
 			int row =  (*it) / n;
 			int col = (*it) % n;
-			//Set scoreDelta
+			//TODO: Set scoreDelta
 			float scoreDelta;
 			score_movements.push_back(new triple(scoreDelta,row,col));
 		}
